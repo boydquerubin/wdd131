fbf055cdee142df9bfff11953e5a4f996572157e
Boyd Querubin <boydisaacq@gmail.com>
2025-01-29 16:35:49 -0700

    act

---
+++ week04/scripts/courses.js
@@ -0,0 +1,27 @@
+const aCourse = {
+  code: "CSE121b",
+  name: "Javascript Language",
+  sections: [
+    {
+    sectionNum: 1,
+      roomNum: "STC 353",
+      enrolled: 26,
+      days: "TTh",
+      instructor: "Bro T",
+    }, 
+    {
+      sectionNum: 2,
+      roomNum: "STC 347",
+      enrolled: 25,
+      days: "TTh",
+      instructor: "Sis A",
+    },
+  ]
+};
+
+function nameAndNumber(course) {
+  const courseName  = document.querySelector("#courseName");
+  const courseCode = document.querySelector("#courseCode");
+  courseName.textContent = course.name;
+  courseCode.textContent = course.code;
+}
