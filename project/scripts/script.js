fc42ff6169e4d2553e0db4a1c1f16b02a60a72a9
Boyd Querubin <boydisaacq@gmail.com>
2025-02-14 20:15:53 -0700

    css

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -194,3 +194,17 @@ function updatePopularityLabel(selectedText) {
   }
 }
 
+function countReview() {
+  let reviewCount = localStorage.getItem("reviewCounter") || 0;
+  reviewCount = parseInt(reviewCount) + 1;
+  localStorage.setItem("reviewCounter", reviewCount);
+
+  let counter = document.getElementById("counter");
+  if (counter) {
+    counter.textContent = reviewCount;
+  }
+}
+
+if (window.location.pathname.includes("review.html")) {
+  countReview();
+}
